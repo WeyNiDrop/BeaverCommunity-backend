14aa7ccd2e898101a800ee2cd9de95c265f52161
evan <evan@gmail.com>
2023-10-15 17:38:26 +0800

    repair bugs

--- src/main/java/com/macro/mall/tiny/modules/web3/contracts/BeaverCommunityEvents.java
+++ src/main/java/com/macro/mall/tiny/modules/web3/contracts/BeaverCommunityEvents.java
@@ -5,8 +5,8 @@ public interface BeaverCommunityEvents {
     String NewComment = "0x6b1ac3ba05874fba053001627afb841ce9af05a8b860ac55a31ee417a2738297";
     String VoteInCompetition = "0x2f1df5bf1b65a1ec4a3d040efd0d4601170f11c9e121f664a98cff730b6b7990";
     String VoteOutCompetition = "0x4baf5b4d45195b76e90d34cf00212cab535e2b24f840cecf411e7aeec7f69197";
-    String WithdrawRewards = "0xd030eeba227c3da3e13caa2eece747dabd9a0a50f4cc4436df66e4e41f39fddb";
-    String BatchWithdrawRewards = "0x93dd29733f7a730d27e925c382690a016b3822f139998e207e7a2c7377b4512f";
+    String WithdrawRewards = "0x93dd29733f7a730d27e925c382690a016b3822f139998e207e7a2c7377b4512f";
+    String BatchWithdrawRewards = "0xd030eeba227c3da3e13caa2eece747dabd9a0a50f4cc4436df66e4e41f39fddb";
     String WithdrawRoyalties = "0xef4eaead1728e863045add578d25280a5dde0e00f0cefdc2ab5645fce1db21a1";
     String BatchWithdrawRoyalties = "0xe4d04c63162ea54558f83892a310aa4cba29359cfdbba61a8d1045ddf031fb94";
 }
